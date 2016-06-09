b85c2a60b193696dce3a4d0d4f26c4ae1e4bfa24
TinnedBread <PJPTaylor@gmx.com>
2016-06-09 19:38:33 +0100

    Some guff

---
+++ Fusion GameJam/Assets/PeteDev/Scripts/Tile.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+
+    //might be better to manually declare which card is placed on the tile
+    // - when card is selected, and tile is clicked, assign selected card as occupant
+
+    [SerializeField]
+    GameObject m_occupant;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	    
+	}
+
+    //needs to be called when the tile is clicked
+    public bool IsOccupied()
+    {
+        if (m_occupant != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    //if mouse is pressed on tile
+    void OnMouseDown()
+    {
+        //- check to see if a card is currently selected (get from singleton)
+        //  - if it is, check if tile is empty (no occupant)
+        //      - if it is, check if tile is within range of structure
+        //          - if it is, place currently selected card on tile (match position)
+        //      - if it is not, do not place card (visual feedback?)
+        //- if no card selected, show detail about the tile/occupant (or make nothing happen)
+        //
+
+        /*
+         * if(GameState.m_instance.isCardSelected == true)
+         * {
+         *      if(!IsOccupied)
+         *      {
+         *          if(GameState.m_instance.resourceCount > GameState.m_instance.GetCurrentCard().GetResourceCost())    //turn into method in Singleton, call it here
+         *          {
+         *              //will need to iterate through each tile
+         *              if(Vector2.Distance(transform.position, structure.transform.position) < threshold)
+         *              {
+         *                  //place card
+         *                  m_occupant = Gamestate.m_instance.GetCurrentCard();
+         *                  //set icon here
+         *              }
+         *          }
+         *      }
+         *      else
+         *      {
+         *          //card cannot be placed (show visual, send event?)
+         *      }
+         * }
+         * else
+         * {
+         *      //show detail about tile being clicked
+         * }
+         * */
+    }
+}
---
+++ Fusion GameJam/Assets/PeteDev/Scripts/Tilemap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilemap : MonoBehaviour {
+
+    /*
+     *  For loop X and Y to make the tile map
+     *  
+     * 
+     * Position camera at 6.5, 3.5, -10
+     * */
+    [SerializeField]
+    GameObject m_tile;
+
+    int m_height, m_width;
+
+	void Start ()
+    {
+        m_height = 8;
+        m_width = 14;
+
+        GenerateMap();  //call method
+	}
+
+    void GenerateMap()
+    {
+        for (int y = 0; y < m_height; y++)
+        {
+            for (int x = 0; x < m_width; x++)
+            {
+                GameObject obj = ObjectPooler.m_current.GetPooledObject();
+
+                if (obj == null)
+                {
+                    return;
+                }
+
+                obj.transform.position = new Vector2(x, y);
+                obj.transform.rotation = transform.rotation;
+                obj.transform.SetParent(gameObject.transform);
+                obj.SetActive(true);
+            }
+        }
+    }
+}

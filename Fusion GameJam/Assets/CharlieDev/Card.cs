51db01865fcfd8fff4f1c31016571c3408e79d78
Csmidget <csmidge2@hotmail.co.uk>
2016-06-09 18:15:21 +0100

    Basic card setup
    - created handmanager
    - created card class

---
+++ Fusion GameJam/Assets/CharlieDev/Card.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card {
+
+	private string name;
+	private GameObject prefab;
+
+
+
+
+	public Card(string name, string desc, )
+	{
+		
+	}
+
+
+
+
+
+}

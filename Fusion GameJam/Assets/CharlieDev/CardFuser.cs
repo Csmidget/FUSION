6a7deb76a49dce25f573e618cb260a77ed296682
Csmidget <csmidge2@hotmail.co.uk>
2016-06-10 00:31:41 +0100

    Recomitting work

---
+++ Fusion GameJam/Assets/CharlieDev/CardFuser.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CardFuser : MonoBehaviour {
+
+	public static CardFuser instance;
+
+	public Card baseCard;
+	public List<Card> modifiers;
+
+	public GameObject baseCardLoc;
+	public GameObject modCardLoc;
+	public GameObject resultLoc;
+
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else {
+			Destroy (this);
+			Debug.Log ("CardFuser instance already exists");
+		}
+
+		foreach (GameObject c in transform) {
+			switch (c.name) 
+			{
+			case "BaseCardLoc":
+				baseCardLoc = c;
+				break;
+			case "ModCardLoc":
+				modCardLoc = c;
+				break;
+			case "ResultLoc":
+				resultLoc = c;
+				break;
+			}
+		}
+
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Fusion GameJam/Assets/CharlieDev/Modifier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Modifier : Card {
+
+	string structurePrefix;
+	string unitPrefix;
+
+	public Modifier (GameObject _prefab, CardType _type, SubType _subType, string _name, Sprite _cardImage, string _cardText, bool _prefabCard,string _structurePrefix,string _unitPrefix, int _range,int _attack,int _defence,int _speed) 
+		: base (_prefab, _type, _subType, _name, _cardImage, _cardText, _prefabCard,_range,_attack,_defence,_speed)
+	{
+		structurePrefix = _structurePrefix;
+		unitPrefix = _unitPrefix;
+	}
+
+
+}

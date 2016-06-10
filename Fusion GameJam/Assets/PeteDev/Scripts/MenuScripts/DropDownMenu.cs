736af1fcdcab354170cbde210e08752ba2b75405
TinnedBread <PJPTaylor@gmx.com>
2016-06-11 00:32:40 +0100

    Main Menu Complete

---
+++ Fusion GameJam/Assets/PeteDev/Scripts/MenuScripts/DropDownMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropDownMenu : MonoBehaviour
+{
+	//start -320, 320 Y
+	//end 0, 0 Y
+
+	//Vector2 m_startPos, m_endPos;
+	RectTransform m_rect;
+
+	void Start ()
+	{
+		m_rect = GetComponent<RectTransform> ();
+	}
+
+	void OnMouseEnter()
+	{
+		//transform.position = Vector2.Lerp(m_startPos, m_endPos
+	}
+
+	void OnMouseExit()
+	{
+
+	}
+}
